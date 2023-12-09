5f2affed5e7c299061927a1efa6a97d166a1edb1
bnjmn <bmcode19@gmail.com>
2023-12-09 18:30:03 +0100

    added changes

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -3,7 +3,7 @@ import { IoCopy } from 'react-icons/io5';
 import { FaChessBoard } from 'react-icons/fa6';
 
 const Advice = () => {
-  const [advice, setAdvice] = useState('');
+  const [advice, setAdvice] = useState({});
   const [showNotification, setShowNotification] = useState(false);
 
   const fetchAdvice = async () => {
@@ -13,15 +13,16 @@ const Advice = () => {
         throw new Error('Failed to fetch advice');
       }
       const adviceData = await data.json();
-      setAdvice(adviceData.slip.advice);
-    } catch (error) {
+      setAdvice(adviceData.slip);
+    }
+     catch (error) {
       console.error('Error fetching advice:', error);
       // Handle error, show a message to the user, etc.
     }
   };
 
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(advice);
+    navigator.clipboard.writeText(advice.advice);
     setShowNotification(true);
 
     // Hide the notification after a certain duration (e.g., 3 seconds)
@@ -50,9 +51,9 @@ const Advice = () => {
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-DarkGrayish rounded-lg">
       <div className="flex justify-start flex-col shadow-2xl md:p-10 p-4 md:w-[475px] w-[275px]">
-        <p className="text-center text-NeonGreen font-light mb-3 text-sm">#Advice {advice.id}</p>
+        <p className="text-center text-NeonGreen font-light mb-3 text-sm">Advice #{advice.id}</p>
 
-        <h1 className="text-[#fff] text-center italic my-3 md:text-[24px] text-[14px]">{`"${advice}" `}</h1>
+        <h1 className="text-[#fff] text-center italic my-3 md:text-[24px] text-[14px]">{`"${advice.advice}"`} </h1>
 
         <div className="h-[1px] w-full text-[#fff] bg-LightCyan my-8 flex items-center justify-center">
           <IoCopy
